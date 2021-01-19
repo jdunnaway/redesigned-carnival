e9e18ea09c3c83c30ea20838c2a51d9b4076c0d7
Justin Dunnaway <justin.dunnaway@candidpartners.com>
2021-01-18 20:09:08 -0500

    adding ecr commands to build image

--- lambda_infrastructure/main.tf
+++ lambda_infrastructure/main.tf
@@ -61,52 +61,11 @@ resource "aws_iam_policy" "lambda_permissions" {
   "Statement": [
     {
       "Action": [
-        "kms:Decrypt"
-      ],
-      "Effect": "Allow",
-      "Resource": "arn:aws:kms:us-east-1:619481458632:key/062e26bc-bb94-4c6e-bea1-f4ea87a6afc3"
-    },
-    {
-      "Action": [
         "logs:CreateLogGroup",
         "logs:CreateLogStream",
         "logs:PutLogEvents"
       ],
       "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    },
-    {
-      "Action": [
-        "dynamodb:GetItem",
-        "dynamodb:PutItem",
-        "dynamodb:QueryItem",
-        "dynamodb:Query",
-        "dynamodb:GetRecords",
-        "dynamodb:GetShardIterator",
-        "dynamodb:DescribeStream",
-        "dynamodb:ListStreams"
-      ],
-      "Resource": ["${aws_dynamodb_table.transaction.arn}","${aws_dynamodb_table.transaction.arn}/*"],
-      "Effect": "Allow"
-    },
-    {
-      "Action": [
-        "xray:PutTraceSegments",
-        "xray:PutTelemetryRecords"
-      ],
-      "Resource": "*",
-      "Effect": "Allow"
-    },
-    {
-      "Action": [
-        "kinesis:GetRecords",
-        "kinesis:PutRecords",
-        "kinesis:PutRecord",
-        "kinesis:GetShardIterator",
-        "kinesis:DescribeStream",
-        "kinesis:ListStreams"
-      ],
-      "Resource": "*",
       "Effect": "Allow"
     }
   ]
@@ -117,53 +76,4 @@ EOF
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role = "${aws_iam_role.stream_consumer_lambda_role.name}"
   policy_arn = "${aws_iam_policy.lambda_permissions.arn}"
-}
-
-resource "aws_kinesis_stream" "transaction_stream" {
-  name             = "transaction-stream"
-  shard_count      = 1
-  retention_period = 24
-
-  tags = {
-    Environment = "dev"
-  }
-}
-
-resource "aws_dynamodb_table" "transaction" {
-  name           = "transaction-dynamodb-table"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 100
-  write_capacity = 100
-  hash_key       = "Id"
-  range_key      = "Sort"
-
-  attribute {
-    name = "Id"
-    type = "S"
-  }
-
-  attribute {
-    name = "Sort"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "transaction-table"
-    Environment = "dev"
-  }
-
-  stream_enabled = true
-  stream_view_type = "KEYS_ONLY"
-}
-
-resource "aws_s3_bucket" "code_bucket" {
-  bucket = "justin-lambda-code-bucket"
-  acl    = "private"
-
-  tags = {
-    Name        = "justins code bucket"
-    Environment = "dev"
-  }
-
-  force_destroy = true
 }
